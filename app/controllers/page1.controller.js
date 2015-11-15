962b5673ea3251c739d8536cb655d6d89b77adba
Joe Pace <pacejoe+github@gmail.com>
2015-11-14 23:33:55 -0500

    added ng-bind and ng-show examples

--- app/controllers/page1.controller.js
+++ app/controllers/page1.controller.js
@@ -3,4 +3,14 @@
 angular.module('app')
     .controller('Page1Ctrl', function ($scope) {
 
+        $scope.page = 'Page 1';
+
+        $scope.message = 'Welcome to the first of our application';
+
+        $scope.editable = false;
+
+        $scope.enableEditing = function(){
+            $scope.editable = true;
+        };
+
     });
